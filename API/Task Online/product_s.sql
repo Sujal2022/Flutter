7155d5ecd93d2025cda3d76e1ed0343533914cbb
Sujal2022 <120082994+Sujal2022@users.noreply.github.com>
2024-10-17 16:15:57 +0530

    commit

--- API/Task Online/product_s.sql
+++ API/Task Online/product_s.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2024 at 12:29 PM
+-- Generation Time: Oct 17, 2024 at 12:45 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Table structure for table `product_s`
 --
 
-CREATE TABLE `product` (
+CREATE TABLE `product_s` (
   `id` int(11) NOT NULL,
   `pname` varchar(255) NOT NULL,
   `pprice` varchar(255) NOT NULL,
@@ -35,10 +35,10 @@ CREATE TABLE `product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table `product_s`
 --
 
-INSERT INTO `product` (`id`, `pname`, `pprice`, `pdesc`) VALUES
+INSERT INTO `product_s` (`id`, `pname`, `pprice`, `pdesc`) VALUES
 (2, 'b', 'b', 'b');
 
 --
@@ -46,9 +46,9 @@ INSERT INTO `product` (`id`, `pname`, `pprice`, `pdesc`) VALUES
 --
 
 --
--- Indexes for table `product`
+-- Indexes for table `product_s`
 --
-ALTER TABLE `product`
+ALTER TABLE `product_s`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -56,9 +56,9 @@ ALTER TABLE `product`
 --
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT for table `product_s`
 --
-ALTER TABLE `product`
+ALTER TABLE `product_s`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
